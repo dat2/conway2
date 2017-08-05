55f1bd2f7a2824f83e8367d2ba8d1ae54dddeda8
Nicholas Dujay <nickdujay@gmail.com>
2017-08-05 15:30:49 -0400

    remove vector; draw initial

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Lib
     ( run
     ) where
 
-import qualified Data.Vector    as V
+import           Control.Monad
 import           Graphics.Gloss
 import           System.Random
 
@@ -14,21 +15,29 @@ instance Random Cell where
     randomR (a, b) g = case randomR (fromEnum a, fromEnum b) g of
         (r, g') -> (toEnum r, g')
 
-data Grid a = Grid { grid :: V.Vector (V.Vector (Int, Int, Cell)), width :: Int, height :: Int }
+data Grid a = Grid { grid :: [[(Int, Int, Cell)]], width :: Int, height :: Int }
         deriving (Show)
 
 new :: Int -> Int -> IO (Grid Cell)
 new width height = do
-    grid <- V.generateM height (V.generateM width . mkCell)
+    grid <- forM [0..(height - 1)] (forM [0..(width - 1)] . mkCell)
     return Grid { width = width, height = height, grid = grid }
 
 mkCell :: Int -> Int -> IO (Int, Int, Cell)
-mkCell row col = do
-    cell <- getStdRandom random
-    return (row, col, cell)
+mkCell row col = (,,) row col <$> getStdRandom random
+
+squareLength :: Num a => a
+squareLength = 50
+
+draw :: Grid Cell -> Picture
+draw Grid { grid, width, height } = Pictures $ map (Pictures . map cell) grid
+  where cell (row,col,Red)  = Color red (square col row)
+        cell (row,col,Blue) = Color blue (square col row)
+        cell (row,col,Off)  = Color black (square col row)
+        offset v = fromIntegral (v * squareLength) / 2 - squareLength / 2
+        square x y = Translate (fromIntegral (x * squareLength) - offset width) (fromIntegral (y * squareLength) - offset height) $ rectangleSolid squareLength squareLength
 
 run :: Int -> Int -> IO ()
 run w h = do
     initial <- new w h
-    print initial
-    display (InWindow "Nice Window" (200, 200) (10, 10)) black (Circle 80)
+    display (InWindow "Nice Window" (w * squareLength, h * squareLength) (0, 0)) black (draw initial)
